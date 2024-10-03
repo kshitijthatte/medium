f319153a39e441aafb3942e79e0f2444e7b62a47
Kshitij Thatte <62874526+kshitijthatte@users.noreply.github.com>
2024-10-03 17:50:07 +0530

    initalized DB

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,39 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+  };
+}>();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.post("/api/v1/user/signup", c => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
 
-export default app
+  return c.text("Hello Hono!");
+});
+
+app.post("/api/v1/user/signin", c => {
+  return c.text("Hello Hono!");
+});
+
+app.post("/api/v1/blog", c => {
+  return c.text("Hello Hono!");
+});
+
+app.put("/api/v1/blog", c => {
+  return c.text("Hello Hono!");
+});
+
+app.get("/api/v1/blog", c => {
+  return c.text("Hello Blogs!");
+});
+
+app.get("/api/v1/blog/bu;k", c => {
+  return c.text("Hello Hono!");
+});
+
+export default app;
