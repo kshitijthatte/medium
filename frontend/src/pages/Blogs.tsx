2ad42fca569c482da10cffa73ba8862c8de0b96a
Kshitij Thatte <62874526+kshitijthatte@users.noreply.github.com>
2024-10-16 12:02:41 +0530

    fronted: added blog pages

--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -1,20 +1,33 @@
 import { Appbar } from "../components/Appbar";
 import { BlogCard } from "../components/BlogCard";
+import { useBlogs } from "../hooks";
 export const Blogs = () => {
+  const { loading, blogs } = useBlogs();
+
+  if (loading) {
+    return <div>loading...</div>;
+  }
   return (
     <div>
       <Appbar />
       <div className="flex justify-center">
         <div>
-          <BlogCard
-            id={1}
-            authorName={"John Doe"}
-            title={"Exploring the Unknown: A Journey into the Abyss"}
-            content={
-              "This article delves into the mysteries of the deep sea, uncovering the fascinating creatures and ecosystems that thrive in the dark. Join us as we explore the depths of the ocean and discover the secrets it holds."
-            }
-            publishedDate={"2nd Feb 2024"}
-          />
+          {blogs.map(blog => (
+            <BlogCard
+              key={blog.id}
+              id={blog.id}
+              authorName={blog.author.name || "Anonymous"}
+              title={blog.title}
+              content={blog.content}
+              publishedDate={String(
+                new Date(
+                  new Date(2020, 0, 1).getTime() +
+                    Math.random() *
+                      (new Date().getTime() - new Date(2020, 0, 1).getTime())
+                )
+              ).slice(4, 15)}
+            />
+          ))}
         </div>
       </div>
     </div>
