fce7644e79cabf7985c4f7ddd996f2b68b377072
Kshitij Thatte <62874526+kshitijthatte@users.noreply.github.com>
2024-10-16 12:38:17 +0530

    frontend: added loaders

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -1,12 +1,17 @@
 import { useParams } from "react-router-dom";
 import { useBlog } from "../hooks";
 import { FullBlog } from "../components/FullBlog";
+import { BlogSkeleton } from "../components/BlogSkeleton";
 
 export const Blog = () => {
   const { id } = useParams();
   const { loading, blog } = useBlog({ id: id || "" });
-  if (loading) {
-    return <div>loading...</div>;
+  if (loading || !blog) {
+    return (
+      <div>
+        <BlogSkeleton />
+      </div>
+    );
   }
   return (
     <div>
